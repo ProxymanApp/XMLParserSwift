5398df9530b23cea24752f3da4adcdef42eb128a
Hang Nguyen <hanu.nguyenhang@gmail.com>
2023-08-11 14:40:52 +0700

    Revert "Add commit hash for package"
    This reverts commit 7e9db97ced47353762cbde18e321d5017dffada0.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ProxymanApp/XMLParserSwift", revision: "da8ce4088d632372b10f33f5415b99465c11b100"),
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
